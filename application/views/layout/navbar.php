c950d9b552f5f9f9be23a3d108bb16e0687f9dff
GabrielNunesVonsnievski-py <gabriel_nunes2017@outlook.com>
2025-04-15 17:55:18 -0300

    15/04/2025

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -2,21 +2,41 @@
                 <div class="container-fluid">
                     <div class="d-flex justify-content-between">
                         <div class="top-menu d-flex align-items-center">
-                        </div>
-                        <span class="text-white"><strong>TREINAMENTOS</strong></span>
-                        <div class="top-menu d-flex align-items-center">
-                            
-                            <button type="button"  id="apps_modal_btn" data-toggle="modal" data-target="#appsModal"><i class="ik ik-grid text-white"></i></button>
-                   
                             <div class="dropdown">
-                                <a class="dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="ik ik-user ik-2x text-white bg-dark"></i></a>
+                                <a class="header-dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa-solid fa-square-phone text-white f-30"></i></a>
+                                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="userDropdown">
+                                    <a data-toggle="tooltip" data-placement="left" title="Entrar em contato" class="dropdown-item" href=""><i class="fa-solid fa-paper-plane dropdown-icon"></i> Entrar em contato</a>
+                                    <a data-toggle="tooltip" data-placement="left" title="Trabalhe conosco" class="dropdown-item" href=""><i class="fa-solid fa-user-tie dropdown-icon"></i> Trabalhe conosco</a>
+                                </div>
+                            </div>
+                        </div>
+                                                
+                        <div class="top-menu d-flex align-items-center">
+                            <button type="button"  id="apps_modal_btn" data-toggle="modal" data-target="#appsModal" class="btn-icon"><i class="fa-solid fa-bars text-white f-20"></i></button>
+                            <div class="dropdown">
+                                <a class="header-dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="true"><i class="ik ik-user ik-2x text-white"></i></a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                    <a data-toggle="tooltip" data-placement="left" title="Gerenciar perfil" class="dropdown-item" href=""><i class="ik ik-user dropdown-icon"></i> Profile</a>
+                                    <a data-toggle="tooltip" data-placement="left" title="Gerenciar perfil" class="dropdown-item" href=""><i class="ik ik-user dropdown-icon"></i> Perfil</a>
                                     <a data-toggle="tooltip" data-placement="left" title="Encerrar a sessão" class="dropdown-item" href=""><i class="ik ik-power dropdown-icon"></i> Sair</a>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </header>
+
+<style>
+
+.btn-icon {
+    background: none;
+    border: none;
+    outline: none;
+    padding: 0;
+    margin: 0;
+}   
+
+.btn-icon:hover {
+    transform: scale(1.5);
+}
+    
+</style>
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,3 +1,4 @@
+<!-- Sidebar -->
 <div class="app-sidebar bg-light">
     <div class="sidebar-header bg-dark">
         <a class="header-brand" href="<?php echo base_url('/') ?>">
@@ -6,92 +7,123 @@
             </div>
         </a>
     </div>
-    
-    <div class="sidebar-content ">
-        <div class="nav-container">
-            <nav id="main-menu-navigation" class="navigation-main">
 
-                <div class="nav-lavel bg-dark text-center text-white"><strong>CURSOS</strong></div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="HTML" href=""><i class="fa-brands fa-html5"></i><span>HTML</span></a>
+    <div class="sidebar-content">
+        <div class="nav-container">
+            <nav id="main-menu-navigation" class="navigation-main">                
+
+                <div class="nav-lavel bg-dark text-center text-white"><strong>Cursos</strong></div>
+
+                <!-- FRONT-END -->
+                <div class="nav-item dropdown-toggle" onclick="toggleDropdown('frontend')">
+                    <div class="nav-lavel bg-dark text-center text-white"><strong>Front-End</strong></div>
                 </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="CSS" href=""><i class="fa-brands fa-css3-alt"></i><span>CSS</span></a>
+                <div id="frontend" class="dropdown-content">
+                    <a href=""><i class="fa-brands fa-html5"></i> HTML</a>
+                    <a href=""><i class="fa-brands fa-css3-alt"></i> CSS</a>
+                    <a href=""><i class="fa-brands fa-js"></i> JavaScript</a>
+                    <a href=""><i class="fa-brands fa-react"></i> React</a>
+                    <a href=""><i class="fa-brands fa-figma"></i> Figma</a>
                 </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="JS" href=""><i class="fa-brands fa-js"></i><span>JavaScript</span></a>
+
+                <!-- BACK-END -->
+                <div class="nav-item dropdown-toggle" onclick="toggleDropdown('backend')">
+                    <div class="nav-lavel bg-dark text-center text-white"><strong>Back-End</strong></div>
                 </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="PHP" href=""><i class="fa-brands fa-php"></i><span>PHP</span></a>
+                <div id="backend" class="dropdown-content">
+                    <a href=""><i class="fa-brands fa-php"></i> PHP</a>
+                    <a href=""><i class="fa-brands fa-java"></i> Java</a>
+                    <a href=""><i class="fa-brands fa-python"></i> Python</a>
+                    <a href=""><i class="fa-brands fa-node-js"></i> Node JS</a>
+                    <a href=""><i class="fa-solid fa-c"></i> C# </a>
                 </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="REACT" href=""><i class="fa-brands fa-react"></i><span>React</span></a>
+
+                <!-- BANCO DE DADOS -->
+                <div class="nav-item dropdown-toggle" onclick="toggleDropdown('Banco de dados')">
+                    <div class="nav-lavel bg-dark text-center text-white"><strong>Banco de dados</strong></div>
                 </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="JAVA" href=""><i class="fa-brands fa-java"></i><span>Java</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="PYTHON" href=""><i class="fa-brands fa-python"></i><span>Python</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="PYTHON" href=""><i class="fa-solid fa-brain"></i><span>IA</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="CYBER" href=""><i class="fa-solid fa-user-secret"></i><span>Cyber Security</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="EXCEL" href=""><i class="fa-solid fa-file-excel"></i><span>Excel</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="WORD" href=""><i class="fa-solid fa-file-word"></i><span>Word</span></a>
-                </div>
-                <div class="nav-item "> 
-                    <a data-toggle="tooltip" data-placement="bottom" title="SQL" href=""><i class="fa-solid fa-database"></i><span>SQL</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="CSHARP" href=""><i class="fa-solid fa-c"></i><span>C sharp</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="C++" href=""><i class="fa-solid fa-c"></i><span>C ++</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="SQL" href=""><i class="fa-brands fa-node-js"></i><span>Node JS</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="SQL" href=""><i class="fa-brands fa-node-js"></i><span>Figma</span></a>
-                </div>
-                <div class="nav-item ">
-                    <a data-toggle="tooltip" data-placement="bottom" title="SQL" href=""><i class="fa-brands fa-node-js"></i><span></span></a>
+                <div id="Banco de dados" class="dropdown-content">
+                    <a href=""><i class="fa-solid fa-database"></i> SQL</a>
+                    <a href=""><i class="fa-brands fa-mdb f-15"></i> Mongo DB</a>
+                    <a href=""><i class="fa-solid fa-fire"></i> Fire Base</a>
                 </div>
                 
+                <!-- OUTROS -->
+                <div class="nav-item dropdown-toggle" onclick="toggleDropdown('outros')">
+                    <div class="nav-lavel bg-dark text-center text-white"><strong>Outros</strong></div>
+                </div>
+                <div id="outros" class="dropdown-content">
+                    <a href=""><i class="fa-solid fa-brain"></i> IA</a>
+                    <a href=""><i class="fa-solid fa-user-secret"></i> Cyber Security</a>
+                    <a href=""><i class="fa-solid fa-file-excel"></i> Excel</a>
+                    <a href=""><i class="fa-solid fa-file-word"></i> Word</a>
+                </div>
 
-                <div class="nav-lavel bg-dark text-center text-white"><strong>ADMINISTRAÇÃO</strong></div>
-                <div class="nav-item">
-                    <a data-toggle="tooltip" data-placement="bottom" title="Gerenciar Alunos" href="<?= base_url('alunos') ?>"><i class="fas fa-users"></i><span>Assinantes</span></a>
+                <!-- ADMIN -->
+                <div class="nav-item dropdown-toggle" onclick="toggleDropdown('Administração')">
+                <div class="nav-lavel bg-dark text-center text-white"><strong>Administração</strong></div>
                 </div>
-                <div class="nav-item">
-                    <a data-toggle="tooltip" data-placement="bottom" title="Gerenciar Precificações" href="<?= base_url('') ?>"><i class="fa-solid fa-money-check-dollar"></i><span>Precificações</span></a>
+                <div id="Administração" class="dropdown-content">
+                    <a href=""><i class="nav-item fa-solid fa-users"></i> Alunos</a>
+                    <a href=""><i class="fa-solid fa-book-open"></i> Aulas</a>
+                    <a href=""><i class="fa-solid fa-clock"></i> Horários</a>
                 </div>
-             
-                <div class="nav-lavel bg-dark text-center text-white"><strong>AULAS</strong></div>
-
-                <div class="nav-item">
-                    <a data-toggle="tooltip" data-placement="bottom" title="Aulas" href=""><i class="fa-solid fa-book-open"></i><span>Aulas</span></a>
-                </div>
-                <div class="nav-item">
-                    <a data-toggle="tooltip" data-placement="bottom" title="Horários" href=""><i class="fa-solid fa-clock"></i><span>Horários</span></a>
-                </div>
-    
-       
             </nav>
         </div>
     </div>
 </div>
 
+
+
 <style>
-    .header-brand-img{
-        width: 100px;
-        max-height: 100px;
-    }
+
+.header-brand-img {
+    width: 100px;
+    max-height: 100px;
+}
+
+.dropdown-content {
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.4s ease-in-out;
+    padding-left: 20px;
+    display: flex;
+    flex-direction: column;
+}
+
+.dropdown-content.show {
+    max-height: 500px; 
+}
+
+.dropdown-content a {
+    padding: 5px 0;
+    color: #333;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+}
+
+.dropdown-toggle {
+    cursor: pointer;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: #000;
+}
+
+.dropdown-toggle:hover {
+    background-color:rgb(214, 214, 214);
+}
 
 </style>
+
+<script>
+    function toggleDropdown(id) {
+        const section = document.getElementById(id);
+        section.classList.toggle('show');
+    }
+</script>
+
+    
